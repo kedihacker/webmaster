94746022c9b2c7b448307160c9a2339ae35cd964
kedi hacker <8588261-kedihacker@users.noreply.gitlab.com>
2022-05-20 20:47:45 +0300

    help is neede

--- webrtc/chat.js
+++ webrtc/chat.js
@@ -1,3 +1,12 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const servers = {
     iceServers: [
         {
@@ -6,23 +15,67 @@ const servers = {
     ],
     iceCandidatePoolSize: 10,
 };
-const pc2 = new RTCPeerConnection(servers);
-const pc1 = new RTCPeerConnection(servers);
+class Queue {
+    constructor() {
+        this.queue = [];
+    }
+    addEventListener(message) {
+        this.callback = message;
+    }
+    send(item) {
+        this.queue.push(item);
+        if (this.callback) {
+            this.callback(this.remove());
+        }
+    }
+    remove() {
+        return this.queue.shift();
+    }
+    size() {
+        return this.queue.length;
+    }
+}
+let alice = null;
+let bob = null;
 let localStream1 = null;
 let remoteStream1 = null;
 let localStream2 = null;
 let remoteStream2 = null;
 let offer1 = null;
-pc1.createOffer().then((offer) => {
-    console.log(offer);
-    pc1.setLocalDescription(offer);
-    offer1 = offer;
-});
-pc2.setRemoteDescription({
-    type: "offer",
-    sdp: offer1.sdp,
-});
-pc1.onicecandidate = (e) => {
-    console.log(e);
-};
+// // Set up an asynchronous communication channel that will be
+// // used during the peer connection setup
+const signalingChannel = new Queue();
+// signalingChannel.addEventListener((message) => {
+//   console.log("Received message from signaling channel:", message);
+// });
+// // Send an asynchronous message to the remote client
+// signalingChannel.send("Hello!");
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+function makeCall() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const configuration = {
+            iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+        };
+        alice = new RTCPeerConnection(configuration);
+        const offer = yield alice.createOffer();
+        yield alice.setLocalDescription(offer);
+        // signalingChannel.send({ offer });
+        bob = new RTCPeerConnection(configuration);
+        bob.setRemoteDescription(new RTCSessionDescription(offer));
+        const answer = yield bob.createAnswer();
+        yield bob.setLocalDescription(answer); //
+        //add event listener
+        const remoteDesc = new RTCSessionDescription(answer);
+        yield alice.setRemoteDescription(remoteDesc);
+        alice.addEventListener("connectionstatechange", (event) => {
+            console.log(event);
+        });
+        yield sleep(1000);
+        console.log("elp");
+        console.log(alice.connectionState);
+    });
+}
+makeCall();
 //# sourceMappingURL=chat.js.map
--- webrtc/chat.ts
+++ webrtc/chat.ts
@@ -7,24 +7,72 @@ const servers = {
   iceCandidatePoolSize: 10,
 };
 
-const pc2 = new RTCPeerConnection(servers);
-const pc1 = new RTCPeerConnection(servers);
+class Queue {
+  queue: any[];
+  callback: Function;
+  constructor() {
+    this.queue = [];
+  }
+  addEventListener(message: Function) {
+    this.callback = message;
+  }
+  send(item: any) {
+    this.queue.push(item);
+    if (this.callback) {
+      this.callback(this.remove());
+    }
+  }
+  remove() {
+    return this.queue.shift();
+  }
+  size() {
+    return this.queue.length;
+  }
+}
+
+let alice = null;
+let bob = null;
 let localStream1 = null;
 let remoteStream1 = null;
 let localStream2 = null;
 let remoteStream2 = null;
 let offer1: RTCSessionDescriptionInit = null;
-pc1.createOffer().then((offer): void => {
-  console.log(offer);
-  pc1.setLocalDescription(offer);
-  offer1 = offer;
-});
 
-pc2.setRemoteDescription({
-  type: "offer",
-  sdp: offer1.sdp,
-});
+// // Set up an asynchronous communication channel that will be
+// // used during the peer connection setup
+const signalingChannel = new Queue();
+// signalingChannel.addEventListener((message) => {
+//   console.log("Received message from signaling channel:", message);
+// });
 
-pc1.onicecandidate = (e) => {
-  console.log(e);
-};
+// // Send an asynchronous message to the remote client
+// signalingChannel.send("Hello!");
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+async function makeCall() {
+  const configuration = {
+    iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+  };
+  alice = new RTCPeerConnection(configuration);
+
+  const offer = await alice.createOffer();
+  await alice.setLocalDescription(offer);
+  // signalingChannel.send({ offer });
+  bob = new RTCPeerConnection(configuration);
+  bob.setRemoteDescription(new RTCSessionDescription(offer));
+  const answer = await bob.createAnswer();
+  await bob.setLocalDescription(answer); //
+  //add event listener
+  const remoteDesc = new RTCSessionDescription(answer);
+
+  await alice.setRemoteDescription(remoteDesc);
+  alice.addEventListener("connectionstatechange", (event) => {
+    console.log(event);
+  });
+  await sleep(1000)
+  console.log("elp")
+  console.log(alice.connectionState)
+}
+
+makeCall();
